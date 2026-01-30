23e5276647cf946ec2a6dfa26676c14c8998e369
Anirudh <140832676+aniupa@users.noreply.github.com>
2026-01-31 02:37:41 +0530

    feat: update server port configuration to use environment variable and improve dotenv integration

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import app from "./src/app.js";
 import { connectDB } from "./src/configs/db/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+const port = process.env.PORT || 4000;
 connectDB();
 
-
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("server is running on port 3000");
 });
