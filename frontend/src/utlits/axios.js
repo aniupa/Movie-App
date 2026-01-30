2479237690561263d311db7c410b6bf1011ba01a
Anirudh <140832676+aniupa@users.noreply.github.com>
2026-01-31 03:59:32 +0530

    feat: update axios instance baseURL to include /api path

--- frontend/src/utlits/axios.js
+++ frontend/src/utlits/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  baseURL: import.meta.env.VITE_INSTANCE_BASE_URL,
+  baseURL: `${import.meta.env.VITE_INSTANCE_BASE_URL}/api`,
   withCredentials: true
 });
 
